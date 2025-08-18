2ff1df2ae5d441260bcaf73efb11bfc55b9b5aa0
RoyalSpirit <mettour@yandex.ru>
2025-08-18 08:56:23 +0300

    Chapter 6 added

---
+++ src/main/java/chapter6/MonthConverter.java
@@ -0,0 +1,12 @@
+package chapter6;
+
+public class MonthConverter {
+
+    public static void main(String[] args) {
+
+        System.out.println(Month.getMonths(2));
+        System.out.println(Month.getMonth("January"));
+
+    }
+
+}
